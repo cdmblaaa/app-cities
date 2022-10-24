16b6eed06ce47ff0e3f5eee7424aa283ea4cb110
cdmblaaa <80826211+cdmblaaa@users.noreply.github.com>
2022-10-24 10:56:32 +0800

    Update app-cities.py

--- app-cities.py
+++ app-cities.py
@@ -2,26 +2,13 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-plt.style.use('seaborn')
-
 st.title('World Cites')
 df = pd.read_csv('worldcities.csv')
 
+# note that you have to use 0.0 and 40.0 given that the data type of population is float
+population_filter = st.slider('Minimal Population (Millions):', 0.0, 40.0, 3.6)  # min, max, default
 
-pop_slider = st.sidebar.slider('choose population',0.0,40.0,3.6)
-df = df[df.population>=pop_slider]
-st.map(df)
-
-st.write(df)
-
-#pop plot
-fig, ax = plt.subplots(figsize=(20, 5))
-pop_sum = df.groupby('country')['population'].sum()
-pop_sum.plot.bar(ax=ax)
-st.pyplot(fig)
-
-
-# create a multi select多选
+# create a multi select
 capital_filter = st.sidebar.multiselect(
      'Capital Selector',
      df.capital.unique(),  # options
@@ -32,8 +19,9 @@ form = st.sidebar.form("country_form")
 country_filter = form.text_input('Country Name (enter ALL to reset)', 'ALL')
 form.form_submit_button("Apply")
 
+
 # filter by population
-df = df[df.population >= pop_slider]
+df = df[df.population >= population_filter]
 
 # filter by country
 df = df[df.capital.isin(capital_filter)]
@@ -41,3 +29,16 @@ df = df[df.capital.isin(capital_filter)]
 if country_filter!='ALL':
     df = df[df.country == country_filter]
 
+# show on map
+st.map(df)
+
+# show dataframe
+st.subheader('City Details:')
+st.write(df[['city', 'country', 'population']])
+
+# show the plot
+st.subheader('Total Population By Country')
+fig, ax = plt.subplots(figsize=(20, 5))
+pop_sum = df.groupby('country')['population'].sum()
+pop_sum.plot.bar(ax=ax)
+st.pyplot(fig)
